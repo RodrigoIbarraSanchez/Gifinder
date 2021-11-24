86729004bf5451a5d88471f1694ad7108aad34ff
RoodrigoIbarraSanchez <rodrigoibarrasanchez@gmail.com>
2021-11-23 23:43:59 -0600

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Pikachu"]);
+
+  return (
+    <>
+      <h2 style={{ textAlign: "center" }}>Gifinder</h2>
+      <p style={{ textAlign: "center" }}>Website without styles </p>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ol>
+    </>
+  );
+};
